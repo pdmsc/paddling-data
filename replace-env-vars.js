f196303f71eeb8769b9e9e3a8a6d98fd51d2b219
Pablo Di Meglio <pablodimeglio@Pablos-MacBook-Pro.local>
2024-11-10 11:51:10 -0800

    Replacing env vars on build for prod

--- replace-env-vars.js
+++ replace-env-vars.js
@@ -8,8 +8,8 @@ const envFilePath = path.resolve(__dirname, 'src/environments/environment.prod.t
 let envFileContent = fs.readFileSync(envFilePath, 'utf-8');
 
 // Replace placeholders with environment variables
-envFileContent = envFileContent.replace(/STRAVA_CLIENT_ID_PLACEHOLDER/g, `'${process.env.STRAVA_CLIENT_ID}'`);
-envFileContent = envFileContent.replace(/STRAVA_CLIENT_SECRET_PLACEHOLDER/g, `'${process.env.STRAVA_CLIENT_SECRET}'`);
+envFileContent = envFileContent.replace(/STRAVA_CLIENT_ID_PLACEHOLDER/g, process.env.STRAVA_CLIENT_ID);
+envFileContent = envFileContent.replace(/STRAVA_CLIENT_SECRET_PLACEHOLDER/g, process.env.STRAVA_CLIENT_SECRET);
 
 // Write the modified content back to environment.prod.ts
 fs.writeFileSync(envFilePath, envFileContent);
