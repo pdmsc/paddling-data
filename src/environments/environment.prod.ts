ca03d49a65a0bc5670b4f6b30ec46d1297388044
Pablo Di Meglio <pablodimeglio@Pablos-MacBook-Pro.local>
2024-11-10 12:09:37 -0800

    Adding REDIRECT_URI environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     clientId: 'STRAVA_CLIENT_ID_PLACEHOLDER',
-    clientSecret: 'STRAVA_CLIENT_SECRET_PLACEHOLDER'
-    redirectUri: 'REDIRECT_URI';
+    clientSecret: 'STRAVA_CLIENT_SECRET_PLACEHOLDER',
+    redirectUri: 'REDIRECT_URI'
   };
