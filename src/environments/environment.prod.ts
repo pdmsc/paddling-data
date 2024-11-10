f1a71739ac0aa10fe473c6de326616285e9b3939
Pablo Di Meglio <pablodimeglio@Pablos-MacBook-Pro.local>
2024-11-10 10:23:09 -0800

    Environment Fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,4 @@ export const environment = {
     production: true,
     clientId: process.env['STRAVA_CLIENT_ID'] || '',
     clientSecret: process.env['STRAVA_CLIENT_SECRET'] || '',
-};
+  };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,7 @@
-// src/environments/environment.ts
-import * as localEnv from './local-env.json';
+import localEnv from './local-env.json';
 
 export const environment = {
   production: false,
-  clientId: (localEnv as any).STRAVA_CLIENT_ID || '',
-  clientSecret: (localEnv as any).STRAVA_CLIENT_SECRET || '',
+  clientId: localEnv.STRAVA_CLIENT_ID || '',
+  clientSecret: localEnv.STRAVA_CLIENT_SECRET || '',
 };
